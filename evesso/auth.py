f177cd6ac6dfff4e66c06a1795a20dc068598f9f
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-02-10 01:21:40 -0600

    rewrite auth app from scratch

--- evesso/auth.py
+++ evesso/auth.py
@@ -1,16 +1,49 @@
 import base64
 import hashlib
-import json
 import secrets
-import urllib
+from urllib.parse import urlencode
 import webbrowser
 
 import requests
 
+from .fileio import dump_jwt
 
-def store_jwt(data, file_path):
-    with open(file_path, 'w') as f:
-        json.dump(data, f)
+
+class Auth:
+    def __init__(self, jwt_file_path=None):
+        self.auth_url = 'https://login.eveonline.com/v2/oauth/token'
+        self.jwt_file_path = jwt_file_path or 'jwt.json'
+
+    def get_code(self):
+        return input("Paste code here: ")
+
+    def authorize(self):
+        code_verifier = generate_random_url_safe_string()
+        query_string = {
+            'response_type': 'code',
+            'redirect_uri': 'http://localhost/',
+            'client_id': '994916b9f0bb4048a901238d8f50b732',
+            'scope': 'esi-characters.read_corporation_roles.v1',
+            'code_challenge': generate_challenge(code_verifier),
+            'code_challenge_method': 'S256',
+            'state': 'something-unique'
+        }
+        webbrowser.open(f'https://login.eveonline.com/v2/oauth/authorize/?{urlencode(query_string)}')
+        code = self.get_code()
+        data = {
+            "grant_type": "authorization_code",
+            "client_id": '994916b9f0bb4048a901238d8f50b732',
+            "code": code,
+            "code_verifier": code_verifier
+        }
+
+        headers = {
+            'Content - Type': 'application / x - www - form - urlencoded',
+            'Host': 'login.eveonline.com'
+        }
+        response = requests.post(self.auth_url, data=data, headers=headers)
+        jwt = response.json()
+        dump_jwt(self.jwt_file_path, jwt)
 
 
 def generate_random_url_safe_string(byte_len=32):
@@ -22,60 +55,3 @@ def generate_challenge(verifier):
     m.update(verifier)
     d = m.digest()
     return base64.urlsafe_b64encode(d).decode().replace("=", "")
-
-
-def generate_auth_url(callback_url, client_id, scope, code_challenge):
-    params = {
-        "response_type": "code",
-        "redirect_uri": callback_url,
-        "client_id": client_id,
-        "scope": scope,
-        "state": '0000000000',
-        "code_challenge": code_challenge,
-        "code_challenge_method": "S256"
-    }
-
-    return f"https://login.eveonline.com/v2/oauth/authorize/?{urllib.parse.urlencode(params)}"
-
-
-def get_code_param():
-    return input("Paste the query string 'code' parameter here: ")
-
-
-def authorize(callback_url, client_id, scope, headers, token_url):
-    code_verifier = generate_random_url_safe_string()
-    code_challenge = generate_challenge(code_verifier)
-    auth_url = generate_auth_url(callback_url, client_id, scope, code_challenge)
-    webbrowser.open(auth_url)
-    auth_code = get_code_param()
-
-    data = {
-        "grant_type": "authorization_code",
-        "client_id": client_id,
-        "code": auth_code,
-        "code_verifier": code_verifier
-    }
-
-    response = requests.post(token_url, data=data, headers=headers)
-    return response.json()
-
-
-# def refresh_access_token():
-#     with open(file_path) as f:
-#         jwt = json.load(f)
-#         refresh_token = jwt.get('refresh_token')
-#
-#     url = "https://login.eveonline.com/v2/oauth/token"
-#
-#     data = {
-#         'grant_type': 'refresh_token',
-#         'refresh_token': refresh_token,
-#         'client_id': CLIENT_ID,
-#         'scope': SCOPE,
-#     }
-#
-#     response = requests.post(url, data=data, headers=headers)
-#     store_jwt(response.json())
-
-def main():
-    pass
