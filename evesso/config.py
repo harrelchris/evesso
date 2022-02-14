14eb0f7267dc15649c3b2e2c21c3094b1cf30326
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-02-13 21:50:00 -0600

    Create config.py

---
+++ evesso/config.py
@@ -0,0 +1,9 @@
+HEADERS = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Host': 'login.eveonline.com'
+}
+AUTH_URL = 'https://login.eveonline.com/v2/oauth/authorize/'
+TOKEN_URL = 'https://login.eveonline.com/v2/oauth/token'
+DEFAULT_CALLBACK_URL = 'https://localhost/callback/'
+STATE = 'secret-state'
+DEFAULT_JWT_PATH = 'jwt.json'
