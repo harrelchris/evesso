4ed246309f97d5150df841c04d600253646f1b9a
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-02-13 22:00:53 -0600

    Create refresh.py to handle refreshing access token

---
+++ evesso/refresh.py
@@ -0,0 +1,27 @@
+import requests
+
+from .config import HEADERS, TOKEN_URL
+
+
+def get_refresh_jwt(client_id: str, scope: str, refresh_token: str) -> dict:
+    """Retrieve stored refresh token and send it in request to get updated access token
+
+    :param client_id: str Client ID associated with your application, found at https://developers.eveonline.com/applications
+    :param scope: str a space-delimited string of scopes, found at https://developers.eveonline.com/applications
+    :param refresh_token: str stored refresh_token from authorization request or previous refresh request
+    :return: dict JWT received from authorization server containing access token and refresh token
+    """
+
+    data = {
+        'grant_type': 'refresh_token',
+        'refresh_token': refresh_token,
+        'client_id': client_id,
+        'scope': scope
+    }
+    response = requests.post(TOKEN_URL, data=data, headers=HEADERS)
+    response.raise_for_status()
+    if response.json().get('access_token'):
+        return response.json()
+    else:
+        print(response.json())
+        raise RuntimeError('JWT request failed')
