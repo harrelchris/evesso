54e7d25b4d988f1c9389ea9b12aa9751475466cd
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-03-13 09:15:32 -0500

    remove unused state string

--- evesso/config.py
+++ evesso/config.py
@@ -1,11 +1,8 @@
-import os
-
 HEADERS = {
     'Content-Type': 'application/x-www-form-urlencoded',
     'Host': 'login.eveonline.com'
 }
 AUTH_URL = 'https://login.eveonline.com/v2/oauth/authorize/'
 TOKEN_URL = 'https://login.eveonline.com/v2/oauth/token'
-STATE = os.getenv('STATE') or 'secret-state'
 DEFAULT_CALLBACK_URL = 'http://localhost/'
 DEFAULT_JWT_PATH = 'jwt.json'
